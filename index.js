3d6730a7b0e43bb2052a37f8dcbf3e53b1df5df9
Jhenzel Ponio <jhenzelgaliciaponio@gmail.com>
2025-01-14 11:10:37 +0800

    feat:

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
+const http = require("http");
+const server = http.createServer(app);
+
 app.get('/', (req, res) => {
     res.send("Server is Running");
 });
@@ -25,9 +28,19 @@ const submitplayerForm = require('./API/submit')
 
 app.use("/submit", submitplayerForm);
 
-// Start the server
+// Start the server\
+
+/*
 const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+*/
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
